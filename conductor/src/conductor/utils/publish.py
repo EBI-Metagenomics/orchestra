20eeef99d62507abe2a54d605e8d14577e176fb7
Ankur Kumar <ank@leoank.me>
2021-06-11 05:26:58 +0530

    update util publish function
    Signed-off-by: Ankur Kumar <ank@leoank.me>

--- conductor/src/conductor/utils/publish.py
+++ conductor/src/conductor/utils/publish.py
@@ -2,23 +2,34 @@
 
 from typing import Union
 
+from conductor.configs import get_config
+from conductor.configs.base import BaseConfig
+
 from google.cloud import pubsub_v1
 
+# Initialize default config at module level
+default_config = get_config()
 
-def publish_msg(msg: str, topic_id: str, timeout: Union[float, None]) -> str:
+
+def publish_msg(
+    msg: str,
+    topic_id: str,
+    timeout: Union[float, None] = None,
+    config: BaseConfig = default_config,
+) -> str:
     """Publish msgs on GCP Pub/Sub topics.
 
     Args:
         msg (str): msg to publish
         topic_id(str): topic id to publish on
         timeout(Union[float, None]): time to wait in seconds before giving up
+        config(BaseConfig): Project config to use
 
     Returns:
         str: The message ID
     """
-    project_id = ""
     publisher = pubsub_v1.PublisherClient()
-    topic_path = publisher.topic_path(project_id, topic_id)
+    topic_path = publisher.topic_path(config.GCP_PROJECT_ID, topic_id)
 
     # Msg must be a bytestring
     msg = msg.encode("utf-8")
