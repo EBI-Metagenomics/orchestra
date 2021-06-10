4ad3a52bf69836fbca0b8b9a3baafe6fee3e12b4
Ankur Kumar <ank@leoank.me>
2021-06-11 02:33:53 +0530

    refactor setup script
    Signed-off-by: Ankur Kumar <ank@leoank.me>

--- scripts/setup_env.py
+++ scripts/setup_env.py
@@ -3,8 +3,6 @@
 
 """Setup scripts."""
 
-import shutil
-from os import makedirs
 from pathlib import Path
 from subprocess import call
 
@@ -15,96 +13,16 @@ conductor_path = Path(__file__).parents[1].joinpath("conductor")
 demon_path = Path(__file__).parents[1].joinpath("demon")
 
 
-def download_file(url: str, path: Path) -> None:
-    """Download and save file.
+try:
+    # Install conductor deps
+    call(["poetry", "install"], cwd=conductor_path)
+    # Calling again as a workaround to ensure project root is installed
+    call(["poetry", "install"], cwd=conductor_path)
 
-    Args:
-        url (str): url of the file to download
-        path (Path): path(including file name) to save downloaded file
-    """
-    pool = PoolManager()
+    # Install demon deps
+    call(["poetry", "install"], cwd=demon_path)
+    # Calling again as a workaround to ensure project root is installed
+    call(["poetry", "install"], cwd=demon_path)
 
-    with open(path, "wb") as out:
-        try:
-            response = pool.request("GET", url, preload_content=False)
-            shutil.copyfileobj(response, out)
-        except HTTPError as e:
-            print(e)
-        response.release_conn()
-
-
-# Install conductor deps
-call(["poetry", "install"], cwd=conductor_path)
-# Calling again as a workaround to ensure project root is installed
-call(["poetry", "install"], cwd=conductor_path)
-
-# Install demon deps
-call(["poetry", "install"], cwd=demon_path)
-# Calling again as a workaround to ensure project root is installed
-call(["poetry", "install"], cwd=demon_path)
-
-# Download slurm
-slurm_version = "20.02.7"
-
-makedirs(name=demon_path.joinpath("external/build").absolute, exist_ok=True)
-
-download_file(
-    url=f"https://download.schedmd.com/slurm/slurm-{slurm_version}.tar.bz2",
-    path=demon_path.joinpath(f"external/slurm-{slurm_version}.tar.bz2").absolute,
-)
-
-# extract slurm files
-shutil.unpack_archive(demon_path.joinpath(f"external/slurm-{slurm_version}.tar.bz2"))
-
-# configure, make and install slurm
-
-call(
-    ["configure", f"--prefix={demon_path.joinpath('external/build').absolute}"],
-    cwd=demon_path.joinpath(f"external/slurm-{slurm_version}"),
-)
-
-call(
-    ["make"],
-    cwd=demon_path.joinpath(f"external/slurm-{slurm_version}"),
-)
-
-call(
-    ["make", "install"],
-    cwd=demon_path.joinpath(f"external/slurm-{slurm_version}"),
-)
-
-# Download pyslurm
-
-pyslurm_version = "20.02.0"
-
-download_file(
-    url=f"https://github.com/PySlurm/pyslurm/archive/refs/tags/{pyslurm_version}.tar.gz",
-    path=demon_path.joinpath(f"external/pyslurm-{pyslurm_version}.tar.gz").absolute,
-)
-
-# extract pyslurm files
-shutil.unpack_archive(
-    demon_path.joinpath(f"external/pyslurm-{pyslurm_version}.tar.bz2")
-)
-
-# build pyslurm
-call(
-    [
-        "python",
-        "setup.py",
-        "build",
-        f"--slurm-lib={demon_path.joinpath('external/build/lib').absolute}",
-        f"--slurm-inc={demon_path.joinpath('external/build/include').absolute}",
-    ],
-    cwd=demon_path.joinpath(f"external/pyslurm-{pyslurm_version}"),
-)
-
-# install pyslurm
-call(
-    [
-        "python",
-        "setup.py",
-        "install",
-    ],
-    cwd=demon_path.joinpath(f"external/pyslurm-{pyslurm_version}"),
-)
+except Exception as e:
+    print(e)
