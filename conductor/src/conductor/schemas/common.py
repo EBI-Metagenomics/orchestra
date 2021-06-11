e54a2e494c3a5bc6bf6bb3c59b97268c0872f98d
Ankur Kumar <ank@leoank.me>
2021-06-11 20:25:01 +0530

    add dummy schemas
    Signed-off-by: Ankur Kumar <ank@leoank.me>

---
+++ conductor/src/conductor/schemas/common.py
@@ -0,0 +1,9 @@
+"""Conductor common schemas."""
+
+from pydantic import BaseModel
+
+
+class ResponseSchema(BaseModel):
+    """API response schema."""
+
+    pass
---
+++ conductor/src/conductor/schemas/job/get.py
@@ -0,0 +1,15 @@
+"""Conductor Job GET route schemas."""
+
+from pydantic import BaseModel
+
+
+class JobGetRequest(BaseModel):
+    """Job GET request schema."""
+
+    pass
+
+
+class JobGetResponse(BaseModel):
+    """Job GET response schema."""
+
+    pass
