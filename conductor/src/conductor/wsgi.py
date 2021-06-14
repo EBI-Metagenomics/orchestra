e90087955b83a8d3172f4d6857725e70f025884c
Ankur Kumar <ank@leoank.me>
2021-06-14 17:47:37 +0530

    move wsgi interface
    Signed-off-by: Ankur Kumar <ank@leoank.me>

--- conductor/src/conductor/wsgi.py
+++
@@ -1,7 +0,0 @@
-"""WSGI module."""
-
-from conductor.configs import Config, get_config
-from conductor.server import create_app
-
-default_config = get_config(Config.DEFAULT)
-app = create_app(default_config)
