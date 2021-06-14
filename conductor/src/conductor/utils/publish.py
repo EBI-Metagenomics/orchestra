7d791fcc67dcfd33ebe52d9ed4c2c476ff1e987a
Ankur Kumar <ank@leoank.me>
2021-06-14 20:06:28 +0530

    Add credentials to publish utility function
    Signed-off-by: Ankur Kumar <ank@leoank.me>

--- conductor/src/conductor/utils/publish.py
+++ conductor/src/conductor/utils/publish.py
@@ -1,11 +1,15 @@
 """Utility function to publish messages to GCP Pub/Sub topics."""
 
+import json
 from typing import Union
 
 from conductor.configs import get_config
 from conductor.configs.base import BaseConfig
 
+
+from google.auth import jwt
 from google.cloud import pubsub_v1
+
 
 # Initialize default config at module level
 default_config = get_config()
@@ -28,7 +32,14 @@ def publish_msg(
     Returns:
         str: The message ID
     """
-    publisher = pubsub_v1.PublisherClient()
+    audience = "https://pubsub.googleapis.com/google.pubsub.v1.Publisher"
+    service_account_info = json.load(
+        open(config.GOOGLE_APPLICATION_CREDENTIALS)
+    )  # noqa: E501
+    credentials = jwt.Credentials.from_service_account_info(
+        service_account_info, audience=audience
+    )
+    publisher = pubsub_v1.PublisherClient(credentials=credentials)
     topic_path = publisher.topic_path(config.GCP_PROJECT_ID, topic_id)
 
     # Msg must be a bytestring
