c4c281e8e5fc100fede8728626e453a10ce56531
Ankur Kumar <ank@leoank.me>
2021-06-18 05:48:05 +0530

    Add base cluster interface
    Signed-off-by: Ankur Kumar <ank@leoank.me>

---
+++ demon/src/demon/clusters/base.py
@@ -0,0 +1,33 @@
+"""Base cluster inteface."""
+
+from abc import ABC, abstractmethod
+from typing import List
+
+from demon.jobs.base import BaseJob, JobStatus
+
+
+class BaseCluster(ABC):
+    """Base cluster interface."""
+
+    @abstractmethod
+    def submit_job(self: "BaseCluster", job: BaseJob) -> str:
+        """Submit job to the cluster.
+
+        Args:
+            job (BaseJob): A Job object
+
+        Returns:
+            str: Job ID
+        """
+        pass
+
+    def get_job_status(self: "BaseCluster", job_id: str) -> List[JobStatus]:
+        """Get status of a job by Job.
+
+        Args:
+            job_id (str): ID of the job
+
+        Returns:
+            List[JobStatus]: List of  status of the jobs # noqa DAR202
+        """
+        pass
