d8efe555205346d03ec98e25fde8345664ebe60a
Ankur Kumar <ank@leoank.me>
2021-06-28 14:53:12 +0530

    add celery tasks
    Signed-off-by: Ankur Kumar <ank@leoank.me>

---
+++ demon/src/demon/tasks.py
@@ -0,0 +1,35 @@
+"""Celery tasks."""
+
+from typing import Any
+
+from demon.extentions import DBSession, celery_app
+from demon.schemas.jobs.base import BaseJobDB
+from demon.utils.slurm_callbacks import submit_slurm_job
+
+from sqlalchemy import select
+
+
+@celery_app.task
+def publish_slurm_job_from_db() -> None:
+    """Check for pending tasks in db and publish to slurm."""
+    pending_jobs = []
+    stmt = select(BaseJobDB).where(BaseJobDB.status == "PENDING")
+    with DBSession() as session:
+        pending_jobs = session.execute(stmt).scalars().all()
+        print(f"Pending jobs: {len(pending_jobs)}")
+    for job in pending_jobs:
+        submit_slurm_job(job)
+
+
+@celery_app.on_after_configure.connect
+def setup_periodic_tasks(sender: Any, **kwargs: Any) -> None:
+    """Set up periodic tasks.
+
+    Args:
+        sender (Any): Sender
+        **kwargs (Any): Kwargs
+    """
+    # Calls publish_slurm_job_from_db() every 10 seconds.
+    sender.add_periodic_task(
+        10.0, publish_slurm_job_from_db.s(), name="Submit jobs every 10s"
+    )
