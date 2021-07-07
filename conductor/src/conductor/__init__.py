b33d3286e5646ae7abf06b813c7736e6d7fa98f4
Ankur Kumar <ank@leoank.me>
2021-07-07 12:33:46 +0530

    initialize config globally
    Signed-off-by: Ankur Kumar <ank@leoank.me>

--- conductor/src/conductor/__init__.py
+++ conductor/src/conductor/__init__.py
@@ -2,8 +2,12 @@
 
 from importlib.metadata import PackageNotFoundError, version  # type: ignore
 
+from conductor.configs import get_config
 
 try:
     __version__ = version(__name__)
 except PackageNotFoundError:  # pragma: no cover
     __version__ = "unknown"
+
+# Initialize global config
+global_config = get_config()
