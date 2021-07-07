eb4cc3269b7c24e3c0e8dd352c6113d1918dd7bd
Ankur Kumar <ank@leoank.me>
2021-07-07 12:34:48 +0530

    use global config
    Signed-off-by: Ankur Kumar <ank@leoank.me>

--- conductor/src/conductor/extentions.py
+++ conductor/src/conductor/extentions.py
@@ -4,7 +4,7 @@ from typing import Any
 
 from celery import Celery
 
-from conductor.configs import get_config
+from conductor import global_config
 
 from flask import Flask
 
@@ -12,16 +12,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
-default_config = get_config()
-
-db_engine = create_engine(default_config.get_sql_db_uri())
+db_engine = create_engine(global_config.get_sql_db_uri())
 # Use this session for all db operations in the app
 DBSession = sessionmaker(db_engine)
 
 celery_app = Celery(
     __name__,
-    broker=default_config.CELERY_BROKER_URL,
-    backend=default_config.CELERY_RESULT_BACKEND,
+    broker=global_config.CELERY_BROKER_URL,
+    backend=global_config.CELERY_RESULT_BACKEND,
 )
 
 
