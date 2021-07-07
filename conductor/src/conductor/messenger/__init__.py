264269953f8c348c5a321ffa8b7f6926bfa440cf
Ankur Kumar <ank@leoank.me>
2021-07-07 13:38:15 +0530

    add get_messenger fucntion
    Signed-off-by: Ankur Kumar <ank@leoank.me>

--- conductor/src/conductor/messenger/__init__.py
+++ conductor/src/conductor/messenger/__init__.py
@@ -1,7 +1,10 @@
 """Messenger that delivers and reads msgs from a pub/sub queue."""
 
 from enum import Enum, unique
+from functools import lru_cache
 
+from conductor import global_config
+from conductor.messenger.base import BaseMessenger
 from conductor.messenger.gcp_messenger import GCPMessenger
 
 
@@ -10,3 +13,13 @@ class MessengerEnum(Enum):
     """Messenger enum."""
 
     GCP = GCPMessenger
+
+
+@lru_cache()
+def get_messenger() -> BaseMessenger:
+    """Cache and return Messenger object.
+
+    Returns:
+        BaseMessenger: An instance of a class that inherits BaseMessenger
+    """
+    return MessengerEnum[global_config.MESSENGER].value(global_config)
