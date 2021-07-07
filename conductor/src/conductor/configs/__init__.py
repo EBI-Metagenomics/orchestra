e8cc2598cfa5c0dd1dab5dbecd94934d23f55bf3
Ankur Kumar <ank@leoank.me>
2021-07-07 12:37:05 +0530

    rename Config to ConfigEnum for clarity
    Signed-off-by: Ankur Kumar <ank@leoank.me>

--- conductor/src/conductor/configs/__init__.py
+++ conductor/src/conductor/configs/__init__.py
@@ -7,18 +7,18 @@ from conductor.configs.default import DefaultConfig
 
 
 @unique
-class Config(Enum):
+class ConfigEnum(Enum):
     """Config enum."""
 
     DEFAULT = DefaultConfig
 
 
 @lru_cache()
-def get_config(config: Config = Config.DEFAULT) -> BaseConfig:
+def get_config(config: ConfigEnum = ConfigEnum.DEFAULT) -> BaseConfig:
     """Cache and return config object.
 
     Args:
-        config (Config): Config to load
+        config (ConfigEnum): Config to load
 
     Returns:
         BaseConfig: An instance of a class that inherits BaseConfig
