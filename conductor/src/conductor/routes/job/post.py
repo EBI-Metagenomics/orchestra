6378a4a06ebd04f024758a8a78396f710b722aaa
Ankur Kumar <ank@leoank.me>
2021-07-09 05:13:04 +0530

    add  job api POST route
    Signed-off-by: Ankur Kumar <ank@leoank.me>

---
+++ conductor/src/conductor/routes/job/post.py
@@ -0,0 +1,61 @@
+"""Conductor job POST route."""
+
+from http import HTTPStatus
+
+from conductor.blocs.job import create_job
+from conductor.routes.job import job_bp
+from conductor.schemas.api.job.post import JobCreate, JobPOSTResponse
+
+from flask import Response, make_response, request
+
+from pydantic import ValidationError
+
+from sqlalchemy.exc import SQLAlchemyError
+
+
+@job_bp.post("/")
+def post_job() -> Response:
+    """Post job.
+
+    Returns:
+        Response: Flask response
+    """
+    # Parse json from request
+    try:
+        job_create = JobCreate.parse_obj(request.json)
+    except ValidationError as e:
+        response_body = JobPOSTResponse(
+            msg="json validation failed", errors=e.errors()
+        )  # noqa: E501
+        return make_response(response_body.json(), HTTPStatus.BAD_REQUEST)
+    except Exception:
+        response_body = JobPOSTResponse(
+            msg="unknown error", errors=["unknown internal error"]
+        )
+        return make_response(
+            response_body.json(), HTTPStatus.INTERNAL_SERVER_ERROR
+        )  # noqa: E501
+
+    # ceate job in DB
+    try:
+        job = create_job(job_create)
+    except SQLAlchemyError:
+        response_body = JobPOSTResponse(
+            msg="internal databse error", errors=["internal database error"]
+        )
+        return make_response(
+            response_body.json(), HTTPStatus.INTERNAL_SERVER_ERROR
+        )  # noqa: E501
+    except Exception:
+        response_body = JobPOSTResponse(
+            msg="unknown error", errors=["unknown internal error"]
+        )
+        return make_response(
+            response_body.json(), HTTPStatus.INTERNAL_SERVER_ERROR
+        )  # noqa: E501
+
+    # return created job in response
+    response_body = JobPOSTResponse(
+        msg="job successfully created", items=[job]
+    )  # noqa: E501
+    return make_response(response_body.json(), HTTPStatus.OK)
