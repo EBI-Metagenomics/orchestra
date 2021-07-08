f385e2f146b73f80c54c8412f86118e5bbb3717b
Ankur Kumar <ank@leoank.me>
2021-07-09 05:12:49 +0530

    add  job api GET route
    Signed-off-by: Ankur Kumar <ank@leoank.me>

--- conductor/src/conductor/routes/job/get.py
+++ conductor/src/conductor/routes/job/get.py
@@ -1,11 +1,61 @@
 """Conductor job GET route."""
 
-from conductor.routes.job import job_bp
+from http import HTTPStatus
 
-from flask import Response
+from conductor.blocs.job import get_jobs
+from conductor.routes.job import job_bp
+from conductor.schemas.api.job.get import JobGetQueryParams, JobGetResponse
+
+from flask import Response, make_response, request
+
+from pydantic.error_wrappers import ValidationError
+
+from sqlalchemy.exc import SQLAlchemyError
 
 
 @job_bp.get("/")
 def get_job() -> Response:
-    """Get job."""
-    pass
+    """Get job.
+
+    Returns:
+        Response: Flask response
+    """
+    # Parse query params from request
+    try:
+        query_params = JobGetQueryParams.parse_raw(request.query_string)
+    except ValidationError as e:
+        response_body = JobGetResponse(
+            msg="query validation error", errors=e.errors()
+        )  # noqa: E501
+        return make_response(response_body, HTTPStatus.BAD_REQUEST)
+    except Exception:
+        response_body = JobGetResponse(
+            msg="unknown error", errors=["unknown internal error"]
+        )
+        return make_response(
+            response_body.json(), HTTPStatus.INTERNAL_SERVER_ERROR
+        )  # noqa: E501
+
+    # Get jobs from the DB
+    try:
+        jobs = get_jobs(query_params)
+    except SQLAlchemyError:
+        response_body = JobGetResponse(
+            msg="internal databse error", errors=["internal database error"]
+        )
+        return make_response(
+            response_body.json(), HTTPStatus.INTERNAL_SERVER_ERROR
+        )  # noqa: E501
+    except Exception:
+        response_body = JobGetResponse(
+            msg="unknown error", errors=["unknown internal error"]
+        )
+        return make_response(
+            response_body.json(), HTTPStatus.INTERNAL_SERVER_ERROR
+        )  # noqa: E501
+
+    # return fetched jobs in response
+    response_body = JobGetResponse(
+        msg="job successfully retrieved", items=jobs
+    )  # noqa: E501
+    return make_response(response_body, HTTPStatus.OK)
