4ef57be79cdd9fb92f5fe92aea42d802c201b52a
Ankur Kumar <ank@leoank.me>
2021-07-19 10:50:22 +0530

    add initial implementation of message schema
    Signed-off-by: Ankur Kumar <ank@leoank.me>

--- conductor/src/conductor/schemas/message.py
+++ conductor/src/conductor/schemas/message.py
@@ -1,9 +1,22 @@
 """Message schema."""
 
+from enum import Enum, unique
+from typing import Dict
+
 from pydantic import BaseModel
+
+
+@unique
+class MessageType(Enum):
+    """Message types."""
+
+    TO_DEMON_SCHEDULE_MSG = "to_demon_schedule_msg"
+    TO_CONDUCTOR_JOB_STATUS_UPDATE = "to_conductor_job_status_update"
 
 
 class Message(BaseModel):
     """Message schema to serialize/deserialze messenger msgs."""
 
-    pass
+    msg_type: MessageType
+    data: Dict
+    timestamp: str
