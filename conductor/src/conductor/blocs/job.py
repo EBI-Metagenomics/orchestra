00038cd72e6b1a20f668363eda594e651e8771df
Ankur Kumar <ank@leoank.me>
2021-07-22 17:46:16 +0530

    add create_job bloc logic
    Signed-off-by: Ankur Kumar <ank@leoank.me>

--- conductor/src/conductor/blocs/job.py
+++ conductor/src/conductor/blocs/job.py
@@ -1,13 +1,22 @@
 """Job BLoCs."""
 
+from datetime import datetime
 from typing import List
 
+from conductor import global_config
+from conductor.extentions import messenger
+from conductor.models.job import JobDB
+from conductor.schemas.api.job.delete import JobDelete
 from conductor.schemas.api.job.get import JobGetQueryParams
 from conductor.schemas.api.job.post import JobCreate
+from conductor.schemas.api.job.put import JobUpdate
 from conductor.schemas.job import Job
+from conductor.schemas.message import Message, MessageType
+
+from logzero import logger
 
 
-def create_job(job_create: JobCreate) -> Job:
+def create_job(job_create: JobCreate) -> JobDB:
     """Create job in the DB from JobCreate request.
 
     Args:
@@ -16,7 +25,22 @@ def create_job(job_create: JobCreate) -> Job:
     Returns:
         JobDB: Instance of Job
     """
-    pass
+    try:
+        job = JobDB(**job_create.job.dict())
+
+        # try to publish msg before commiting
+        message = Message(
+            msg_type=MessageType.TO_DEMON_SCHEDULE_MSG,
+            data=job_create.job.dict(),
+            timestamp=datetime.now(),
+        )
+        messenger.publish(message, global_config.GCP_PUBSUB_TOPIC)
+
+        # Commit to db and return job
+        job.save()
+        return job
+    except Exception as e:
+        logger.error(f"Unable to schedule msg: {e}")
 
 
 def get_jobs(query_params: JobGetQueryParams) -> List[Job]:
@@ -27,5 +51,29 @@ def get_jobs(query_params: JobGetQueryParams) -> List[Job]:
 
     Returns:
         List[Job]: List of jobs returned from DB
+    """
+    pass
+
+
+def update_job(job_update: JobUpdate) -> Job:
+    """Update job in the DB from JobUpdate request.
+
+    Args:
+        job_update (JobUpdate): JobUpdate request
+
+    Returns:
+        JobDB: Instance of Updated Job
+    """
+    pass
+
+
+def delete_job(job_delete: JobDelete) -> Job:
+    """Delete job in the DB from JobDelete request.
+
+    Args:
+        job_delete (JobDelete): JobDelete request
+
+    Returns:
+        JobDB: Instance of Deleted Job
     """
     pass
