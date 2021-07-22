bb73c8a41bf1e68c8ee74862f4c5c66316b60c5e
Ankur Kumar <ank@leoank.me>
2021-07-22 17:46:35 +0530

    add schedule cli command
    Signed-off-by: Ankur Kumar <ank@leoank.me>

---
+++ conductor/src/conductor/cli/schedule.py
@@ -0,0 +1,35 @@
+"""Schedule commands."""
+# flake8:
+
+from pathlib import Path
+
+import click
+
+from conductor.blocs.job import create_job
+from conductor.schemas.api.job.post import JobCreate
+from conductor.schemas.job import Job
+
+from logzero import logger
+
+
+@click.command()
+@click.option("--file", help="file path of job's JSON file", required=True)
+def random(file: str) -> None:
+    """Schedule a job."""
+    logger.info("Trying to schedule the job...")
+    try:
+        job_file_path = Path(file)
+        with open(job_file_path) as job_file:
+            data = job_file.read()
+            job = Job().parse_raw(data)
+            job_create = JobCreate(job=job)
+            created_job = create_job(job_create)
+            logger.info(f"\nSuccess!\nJob: {created_job}")
+    except Exception as e:
+        logger.error(f"failed to schedule the job: {e}")
+
+
+@click.group()
+def sched() -> None:
+    """Schedule jobs."""
+    pass
