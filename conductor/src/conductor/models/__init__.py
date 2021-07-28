561c7fc898ce03dfcab977c2305886132988f642
Ankur Kumar <ank@leoank.me>
2021-07-28 16:42:47 +0530

    fix models creation issue
    Signed-off-by: Ankur Kumar <ank@leoank.me>

--- conductor/src/conductor/models/__init__.py
+++ conductor/src/conductor/models/__init__.py
@@ -1,1 +1,6 @@
 """Conductor models."""
+
+from conductor.models.cluster import ClusterDB  # noqa: F401
+from conductor.models.job import JobDB  # noqa: F401
+from conductor.models.protagonist import ProtagonistDB  # noqa: F401
+from conductor.models.schedule import ScheduleDB  # noqa: F401
