39b0895cd4f2c574c0ddbe6fe6ea66dc0ca40173
Ankur Kumar <ank@leoank.me>
2021-07-28 16:41:45 +0530

    add schedule db model
    Signed-off-by: Ankur Kumar <ank@leoank.me>

---
+++ conductor/src/conductor/models/schedule.py
@@ -0,0 +1,25 @@
+"""Schedule DBModel."""
+
+
+from conductor.models.meta.mixins import (
+    DBModel,
+    SurrogatePKUUID,
+    TimestampMixin,
+)  # noqa: E501
+from conductor.models.meta.orm import reference_col
+
+from sqlalchemy import Column, DateTime
+from sqlalchemy.orm import relationship
+
+
+class ScheduleDB(DBModel, TimestampMixin, SurrogatePKUUID):
+    """Schedule table."""
+
+    __tablename__ = "schedule"
+    job_id = reference_col("job")
+    job = relationship("JobDB", backref="schedules")
+    assigned_cluster_id = reference_col("cluster")
+    assigned_cluster = relationship("ClusterDB", backref="schedules")
+    finished_at = Column(DateTime(timezone=True), nullable=True)
+    protagonist_id = reference_col("protagonist")
+    protagonist = relationship("ProtagonistDB", backref="schedules")
