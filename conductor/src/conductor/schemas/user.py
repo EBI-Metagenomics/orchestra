02599f6672788e79ff54d5fe99d170378f9df8ee
Ankur Kumar <ank@leoank.me>
2021-08-02 07:07:59 +0100

    Add user schema
    Signed-off-by: Ankur Kumar <ank@leoank.me>

---
+++ conductor/src/conductor/schemas/user.py
@@ -0,0 +1,16 @@
+"""User schema."""
+
+from typing import Optional
+
+from pydantic import BaseModel
+from pydantic.types import UUID4
+
+
+class User(BaseModel):
+    """User schema."""
+
+    id: Optional[UUID4]
+    name: str
+    organisation: str
+    email: str
+    active: bool
