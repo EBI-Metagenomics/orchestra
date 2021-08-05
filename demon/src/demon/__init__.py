52993ba9829b76476659090aaac9dcd796412993
Ankur Kumar <ank@leoank.me>
2021-08-05 17:52:24 +0100

    refactor demon init -> two stage init
    Signed-off-by: Ankur Kumar <ank@leoank.me>

--- demon/src/demon/__init__.py
+++ demon/src/demon/__init__.py
@@ -1,9 +1,28 @@
-"""Demon"""
+"""Demon."""
 
-from importlib.metadata import version, PackageNotFoundError  # type: ignore
+from importlib.metadata import PackageNotFoundError, version
 
+from celery import Celery
+
+from demon.configs import get_config
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 try:
     __version__ = version(__name__)
 except PackageNotFoundError:  # pragma: no cover
     __version__ = "unknown"
+
+
+default_config = get_config()
+
+db_engine = create_engine(default_config.get_sql_db_uri())
+# Use this session for all db operations in the app
+DBSession = sessionmaker(db_engine)
+
+celery_app = Celery(
+    __name__,
+    broker=default_config.CELERY_BROKER_URL,
+    backend=default_config.CELERY_RESULT_BACKEND,
+)
--- demon/src/demon/extentions.py
+++ demon/src/demon/extentions.py
@@ -1,20 +1,8 @@
 """Demon's celery interface."""
 
-from celery import Celery
+from demon.cluster import get_cluster
+from demon.messenger import get_messenger
 
-from demon.configs import get_config
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-default_config = get_config()
-
-db_engine = create_engine(default_config.get_sql_db_uri())
-# Use this session for all db operations in the app
-DBSession = sessionmaker(db_engine)
-
-celery_app = Celery(
-    __name__,
-    broker=default_config.CELERY_BROKER_URL,
-    backend=default_config.CELERY_RESULT_BACKEND,
-)
+messenger = get_messenger()
+cluster = get_cluster()
