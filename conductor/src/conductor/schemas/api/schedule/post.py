d53ee2b6d903006fc6c8a94a5300f919c0b0e062
Ankur Kumar <ank@leoank.me>
2021-08-05 16:12:40 +0100

    add user to schedule create schema
    Signed-off-by: Ankur Kumar <ank@leoank.me>

--- conductor/src/conductor/schemas/api/schedule/post.py
+++ conductor/src/conductor/schemas/api/schedule/post.py
@@ -1,6 +1,9 @@
 """Conductor Schedule POST route schemas."""
 
+from typing import Optional
+
 from conductor.schemas.schedule import Schedule
+from conductor.schemas.user import User
 
 from pydantic import BaseModel
 
@@ -21,3 +24,4 @@ class ScheduleCreate(BaseModel):
     """Schema to parse create schedule requests."""
 
     schedule: Schedule
+    user: Optional[User]
