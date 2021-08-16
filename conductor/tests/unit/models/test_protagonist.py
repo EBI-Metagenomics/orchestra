fb3d782f21caed91d349124bc00f22d5d457eb0e
Ankur Kumar <ank@leoank.me>
2021-08-16 09:58:33 +0100

    update protagonist unit test
    Signed-off-by: Ankur Kumar <ank@leoank.me>

--- conductor/tests/unit/models/test_protagonist.py
+++ conductor/tests/unit/models/test_protagonist.py
@@ -11,6 +11,25 @@ from sqlalchemy import select
 from sqlalchemy.orm.session import Session
 
 
+def test_get_all_users(db: Session) -> None:
+    passwd = hashpw("random_key".encode(), gensalt())
+    user = ProtagonistDB(
+        name="test_user_1",
+        email="test_email_1@gmail.com",
+        password=passwd,
+    )
+
+    with db() as session:
+        user.save(session)
+        stmt = select(ProtagonistDB)
+        try:
+            fetched_users = session.execute(stmt).scalars().all()
+        except Exception as e:
+            logger.error(f"Unable tofetch user: {e}")
+            raise e
+        assert user.id in [str(u.id) for u in fetched_users]
+
+
 def test_get_by_email(db: Session) -> None:
     passwd = hashpw("random_key".encode(), gensalt())
     user = ProtagonistDB(
@@ -18,11 +37,9 @@ def test_get_by_email(db: Session) -> None:
         email="test_email",
         password=passwd,
     )
+
     with db() as session:
         user.save(session)
-
-    # recreate new session after saving as a workaround
-    with db() as session:
         stmt = select(ProtagonistDB).where(ProtagonistDB.email == "test_email")
         try:
             fetched_user = session.execute(stmt).scalars().all()[0]
