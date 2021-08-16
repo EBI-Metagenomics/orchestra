8a1b343674392935e112dacb12f423be60e8fd8c
Ankur Kumar <ank@leoank.me>
2021-08-16 09:58:16 +0100

    add cluster unit test
    Signed-off-by: Ankur Kumar <ank@leoank.me>

---
+++ conductor/tests/unit/models/test_cluster.py
@@ -0,0 +1,33 @@
+"""Cluster model tests."""
+# flake8: noqa
+
+from typing import Dict
+
+
+from conductor.models.cluster import ClusterDB
+
+from logzero import logger
+
+from sqlalchemy import select
+from sqlalchemy.orm.session import Session
+
+
+def test_get_all_clusters(db: Session) -> None:
+
+    cluster = ClusterDB(
+        name="EBI_Embassy_01",
+        cluster_type="SLURM",
+        status="ACTIVE",
+        cluster_caps="CPU,GPU",
+        messenger="GCP",
+        messenger_queue="test-topic",
+    )
+    with db() as session:
+        cluster.save(session)
+        stmt = select(ClusterDB)
+        try:
+            fetched_clusters = session.execute(stmt).scalars().all()
+        except Exception as e:
+            logger.error(f"Unable to fetch clusters: {e}")
+            raise e
+        assert cluster.id in [str(c.id) for c in fetched_clusters]
