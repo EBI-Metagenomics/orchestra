2b476c7d90f8ef0131b5c12c8d550420fe0f2c7d
Ankur Kumar <ank@leoank.me>
2021-08-20 18:04:25 +0100

    add cli login command
    Signed-off-by: Ankur Kumar <ank@leoank.me>

--- conductor/src/conductor/cli/main.py
+++ conductor/src/conductor/cli/main.py
@@ -1,5 +1,9 @@
 """Command-line interface."""
 
+# flake8: noqa
+
+import os
+
 import click
 
 from conductor.cli.create import create
@@ -7,8 +11,24 @@ from conductor.cli.db import db
 from conductor.cli.get import get
 from conductor.cli.publish import pub
 from conductor.cli.schedule import sched
+from conductor.extentions import auther
+from conductor.schemas.api.auth.post import AuthUserCreds
+
 
 from .. import __version__
+
+
+@click.command()
+@click.option("--email", required=True, help="email of user")
+@click.option("--password", required=True, help="password of user")
+def login(email, password) -> None:
+    auth_creds = AuthUserCreds(email=email, password=password)
+    login_tuple = auther.login_user(auth_creds)
+    if login_tuple is None:
+        click.secho("Login failed!", fg="red")
+    else:
+        click.secho(f"Your token: {login_tuple[1]}")
+        click.secho(f"Adding it to your env...")
 
 
 @click.group()
@@ -23,3 +43,4 @@ main.add_command(get)
 main.add_command(db)
 main.add_command(pub)
 main.add_command(sched)
+main.add_command(login)
