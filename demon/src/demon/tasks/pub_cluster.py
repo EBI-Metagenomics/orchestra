26e2ef48034d46bf75272f3cb2fdad514cd75b20
Ankur Kumar <ank@leoank.me>
2021-08-20 19:57:43 +0100

    refactor publish task
    Signed-off-by: Ankur Kumar <ank@leoank.me>

--- demon/src/demon/tasks/pub_cluster.py
+++ demon/src/demon/tasks/pub_cluster.py
@@ -2,9 +2,10 @@
 
 from typing import List
 
-from demon.extentions import DBSession, celery_app
+from demon import DBSession, celery_app
+from demon.extentions import cluster
 from demon.models.job import JobDB
-from demon.utils.slurm_callbacks import submit_slurm_job
+from demon.schemas.job import Job
 
 from sqlalchemy import select
 
@@ -18,6 +19,8 @@ def publish_slurm_job_from_db() -> None:
         pending_jobs: List[JobDB] = session.execute(stmt).scalars().all()
         print(f"Pending jobs: {len(pending_jobs)}")
         if len(pending_jobs) != 0:
-            submit_slurm_job(pending_jobs[0])
+            cluster.submit_job(
+                Job(job_id=pending_jobs[0].id, **pending_jobs[0].to_dict())
+            )
             pending_jobs[0].update(session, status="SUBMTTED")
             # TODO: Notify conductor that job is submitted
