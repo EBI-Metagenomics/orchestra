e26f1e3d3070a5a28275379c047910a510c1a0e5
Ankur Kumar <ank@leoank.me>
2021-08-20 18:28:39 +0100

    fix global config import in demon
    Signed-off-by: Ankur Kumar <ank@leoank.me>

--- demon/src/demon/__init__.py
+++ demon/src/demon/__init__.py
@@ -15,14 +15,14 @@ except PackageNotFoundError:  # pragma: no cover
     __version__ = "unknown"
 
 
-default_config = get_config()
+global_config = get_config()
 
-db_engine = create_engine(default_config.get_sql_db_uri())
+db_engine = create_engine(global_config.get_sql_db_uri())
 # Use this session for all db operations in the app
 DBSession = sessionmaker(db_engine)
 
 celery_app = Celery(
     __name__,
-    broker=default_config.CELERY_BROKER_URL,
-    backend=default_config.CELERY_RESULT_BACKEND,
+    broker=global_config.CELERY_BROKER_URL,
+    backend=global_config.CELERY_RESULT_BACKEND,
 )
