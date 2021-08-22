05765bafc8c4abbc3f7b7f9683c94ac92c63e0af
Ankur Kumar <ank@leoank.me>
2021-08-22 02:41:18 +0100

    add pub messenger celery task
    Signed-off-by: Ankur Kumar <ank@leoank.me>

---
+++ demon/src/demon/tasks/pub_messenger.py
@@ -0,0 +1,25 @@
+"""Task to publish msgs to GCP Pub/Sub topic."""
+
+from demon import celery_app
+from demon.extentions import messenger
+from demon.schemas.message import Message
+
+from logzero import logger
+
+
+@celery_app.task
+def publish_messenger(msg: Message, topic_id: str) -> None:
+    """Publish msgs to GCP Pub/Sub topic.
+
+    Args:
+        msg (Message): msg to publish
+        topic_id (str): Id of the topic
+
+    Raises:
+        Exception: error
+    """
+    try:
+        messenger.publish(msg=msg, topic_id=topic_id)
+    except Exception as e:
+        logger.error(e)
+        raise e
