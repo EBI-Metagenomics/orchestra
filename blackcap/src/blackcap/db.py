a7f816a96bff7ca48d3630828b45c0d9d9607f98
Ankur Kumar <ank@leoank.me>
2021-10-26 15:36:25 +0100

    use db uri
    Signed-off-by: Ankur Kumar <ank@leoank.me>

--- blackcap/src/blackcap/db.py
+++ blackcap/src/blackcap/db.py
@@ -5,6 +5,6 @@ from blackcap.configs import config_registry
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-db_engine = create_engine(config_registry.get_config().get_sql_db_uri())
+db_engine = create_engine(config_registry.get_config().SQLALCHEMY_DATABASE_URI)
 # Use this session for all db operations in the app
 DBSession = sessionmaker(db_engine)
