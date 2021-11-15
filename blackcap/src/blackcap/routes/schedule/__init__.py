33345e866dc42b3f1895aa43a866ca1e9b02156e
Ankur Kumar <ank@leoank.me>
2021-11-15 17:53:07 +0000

    fix import issue
    Signed-off-by: Ankur Kumar <ank@leoank.me>

--- blackcap/src/blackcap/routes/schedule/__init__.py
+++ blackcap/src/blackcap/routes/schedule/__init__.py
@@ -5,6 +5,6 @@ from flask import Blueprint
 
 schedule_bp = Blueprint("schedule", __name__, url_prefix="/v1/schedule")
 
-from conductor.routes.schedule.get import get_schedule  # noqa: F401, E402, I100, E501
+from blackcap.routes.schedule.get import get_schedule  # noqa: F401, E402, I100, E501
 
-# from conductor.routes.schedule.post import post_schedule  # noqa: F401, E402, I100, E501
+# from blackcap.routes.schedule.post import post_schedule  # noqa: F401, E402, I100, E501
