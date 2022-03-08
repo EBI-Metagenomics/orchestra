3f780e9b9cd4232a1938cc92bfe0b5f7f771f0f3
Ankur Kumar <ank@leoank.me>
2022-03-08 17:48:41 +0000

    update job routes
    Signed-off-by: Ankur Kumar <ank@leoank.me>

--- blackcap/src/blackcap/routes/job/get.py
+++ blackcap/src/blackcap/routes/job/get.py
@@ -2,19 +2,17 @@
 
 from http import HTTPStatus
 
+from flask import make_response, request, Response
+from pydantic.error_wrappers import ValidationError
+from sqlalchemy.exc import SQLAlchemyError
+
 from blackcap.blocs.job import get_jobs
 from blackcap.routes.job import job_bp
 from blackcap.schemas.api.job.get import JobGetQueryParams, JobGetResponse
 
-from flask import Response, make_response, request
-
-from pydantic.error_wrappers import ValidationError
-
-from sqlalchemy.exc import SQLAlchemyError
-
 
 @job_bp.get("/")
-def get_job() -> Response:
+def get() -> Response:
     """Get job.
 
     Returns:
@@ -25,37 +23,31 @@ def get_job() -> Response:
         query_params = JobGetQueryParams.parse_obj(request.args)
     except ValidationError as e:
         response_body = JobGetResponse(
-            msg="query validation error", errors=e.errors()
-        )  # noqa: E501
+            msg="query validation error", errors={"main": e.errors()}
+        )
         return make_response(response_body.json(), HTTPStatus.BAD_REQUEST)
     except Exception:
         response_body = JobGetResponse(
-            msg="unknown error", errors=["unknown internal error"]
+            msg="unknown error", errors={"main": ["unknown internal error"]}
         )
-        return make_response(
-            response_body.json(), HTTPStatus.INTERNAL_SERVER_ERROR
-        )  # noqa: E501
+        return make_response(response_body.json(), HTTPStatus.INTERNAL_SERVER_ERROR)
 
     # Get jobs from the DB
     try:
-        jobs = get_jobs(query_params)
+        job_list = get_jobs(query_params)
     except SQLAlchemyError:
         response_body = JobGetResponse(
-            msg="internal databse error", errors=["internal database error"]
+            msg="internal databse error", errors={"main": ["unknown internal error"]}
         )
-        return make_response(
-            response_body.json(), HTTPStatus.INTERNAL_SERVER_ERROR
-        )  # noqa: E501
+        return make_response(response_body.json(), HTTPStatus.INTERNAL_SERVER_ERROR)
     except Exception:
         response_body = JobGetResponse(
-            msg="unknown error", errors=["unknown internal error"]
+            msg="unknown error", errors={"main": ["unknown internal error"]}
         )
-        return make_response(
-            response_body.json(), HTTPStatus.INTERNAL_SERVER_ERROR
-        )  # noqa: E501
+        return make_response(response_body.json(), HTTPStatus.INTERNAL_SERVER_ERROR)
 
     # return fetched jobs in response
     response_body = JobGetResponse(
-        msg="job successfully retrieved", items=jobs
-    )  # noqa: E501
+        msg="job successfully retrieved", items={"job_list": job_list}
+    )
     return make_response(response_body.json(), HTTPStatus.OK)
--- blackcap/src/blackcap/routes/job/post.py
+++ blackcap/src/blackcap/routes/job/post.py
@@ -1,17 +1,15 @@
 """Blackcap job POST route."""
 
-import json
 from http import HTTPStatus
+import json
+
+from flask import make_response, request, Response
+from pydantic import ValidationError
+from sqlalchemy.exc import SQLAlchemyError
 
 from blackcap.blocs.job import create_job
 from blackcap.routes.job import job_bp
 from blackcap.schemas.api.job.post import JobCreate, JobPOSTResponse
-
-from flask import Response, make_response, request
-
-from pydantic import ValidationError
-
-from sqlalchemy.exc import SQLAlchemyError
 
 
 @job_bp.post("/")
@@ -26,37 +24,31 @@ def post_job() -> Response:
         job_create = JobCreate.parse_obj(json.loads(request.json))
     except ValidationError as e:
         response_body = JobPOSTResponse(
-            msg="json validation failed", errors=e.errors()
-        )  # noqa: E501
+            msg="json validation failed", errors={"main": e.errors()}
+        )
         return make_response(response_body.json(), HTTPStatus.BAD_REQUEST)
     except Exception:
         response_body = JobPOSTResponse(
-            msg="unknown error", errors=["unknown internal error"]
+            msg="unknown error", errors={"main": ["unknown internal error"]}
         )
-        return make_response(
-            response_body.json(), HTTPStatus.INTERNAL_SERVER_ERROR
-        )  # noqa: E501
+        return make_response(response_body.json(), HTTPStatus.INTERNAL_SERVER_ERROR)
 
     # ceate job in DB and publish msg
     try:
-        job = create_job(job_create)
+        job_list = create_job(job_create)
     except SQLAlchemyError:
         response_body = JobPOSTResponse(
-            msg="internal databse error", errors=["internal database error"]
+            msg="internal databse error", errors={"main": ["unknown internal error"]}
         )
-        return make_response(
-            response_body.json(), HTTPStatus.INTERNAL_SERVER_ERROR
-        )  # noqa: E501
+        return make_response(response_body.json(), HTTPStatus.INTERNAL_SERVER_ERROR)
     except Exception:
         response_body = JobPOSTResponse(
-            msg="unknown error", errors=["unknown internal error"]
+            msg="unknown error", errors={"main": ["unknown internal error"]}
         )
-        return make_response(
-            response_body.json(), HTTPStatus.INTERNAL_SERVER_ERROR
-        )  # noqa: E501
+        return make_response(response_body.json(), HTTPStatus.INTERNAL_SERVER_ERROR)
 
     # return created job in response
     response_body = JobPOSTResponse(
-        msg="job successfully created", items=[job.dict()]
-    )  # noqa: E501
+        msg="job successfully created", items={"job_list": job_list}
+    )
     return make_response(response_body.json(), HTTPStatus.OK)
