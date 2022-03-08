cbd0bc83aa871aa8bacadc522a0b1501c12ac545
Ankur Kumar <ank@leoank.me>
2022-03-08 17:48:50 +0000

    update schedule routes
    Signed-off-by: Ankur Kumar <ank@leoank.me>

--- blackcap/src/blackcap/routes/schedule/get.py
+++ blackcap/src/blackcap/routes/schedule/get.py
@@ -2,18 +2,16 @@
 
 from http import HTTPStatus
 
+from flask import make_response, request, Response
+from pydantic.error_wrappers import ValidationError
+from sqlalchemy.exc import SQLAlchemyError
+
 from blackcap.blocs.schedule import get_schedules
 from blackcap.routes.schedule import schedule_bp
 from blackcap.schemas.api.schedule.get import (
     ScheduleGetQueryParams,
     ScheduleGetResponse,
 )
-
-from flask import Response, make_response, request
-
-from pydantic.error_wrappers import ValidationError
-
-from sqlalchemy.exc import SQLAlchemyError
 
 
 @schedule_bp.get("/")
@@ -28,37 +26,31 @@ def get_schedule() -> Response:
         query_params = ScheduleGetQueryParams.parse_raw(request.query_string)
     except ValidationError as e:
         response_body = ScheduleGetResponse(
-            msg="query validation error", errors=e.errors()
-        )  # noqa: E501
+            msg="query validation error", errors={"main": e.errors()}
+        )
         return make_response(response_body.json(), HTTPStatus.BAD_REQUEST)
     except Exception:
         response_body = ScheduleGetResponse(
-            msg="unknown error", errors=["unknown internal error"]
+            msg="unknown error", errors={"main": ["unknown internal error"]}
         )
-        return make_response(
-            response_body.json(), HTTPStatus.INTERNAL_SERVER_ERROR
-        )  # noqa: E501
+        return make_response(response_body.json(), HTTPStatus.INTERNAL_SERVER_ERROR)
 
     # Get s from the DB
     try:
-        schedules = get_schedules(query_params)
+        schedule_list = get_schedules(query_params)
     except SQLAlchemyError:
         response_body = ScheduleGetResponse(
-            msg="internal databse error", errors=["internal database error"]
+            msg="internal databse error", errors={"main": ["unknown internal error"]}
         )
-        return make_response(
-            response_body.json(), HTTPStatus.INTERNAL_SERVER_ERROR
-        )  # noqa: E501
+        return make_response(response_body.json(), HTTPStatus.INTERNAL_SERVER_ERROR)
     except Exception:
         response_body = ScheduleGetResponse(
-            msg="unknown error", errors=["unknown internal error"]
+            msg="unknown error", errors={"main": ["unknown internal error"]}
         )
-        return make_response(
-            response_body.json(), HTTPStatus.INTERNAL_SERVER_ERROR
-        )  # noqa: E501
+        return make_response(response_body.json(), HTTPStatus.INTERNAL_SERVER_ERROR)
 
     # return fetched s in response
     response_body = ScheduleGetResponse(
-        msg=" successfully retrieved", items=schedules
-    )  # noqa: E501
+        msg=" successfully retrieved", items={"schedule_list": schedule_list}
+    )
     return make_response(response_body.json(), HTTPStatus.OK)
--- blackcap/src/blackcap/routes/schedule/post.py
+++ blackcap/src/blackcap/routes/schedule/post.py
@@ -2,60 +2,53 @@
 
 from http import HTTPStatus
 
-from blackcap.blocs.schedule import create_schedule
-from blackcap.routes.job import job_bp
-from blackcap.schemas.api.job.post import JobCreate, JobPOSTResponse
 
-from flask import Response, make_response, request
-
+from flask import make_response, request, Response
 from pydantic import ValidationError
-
 from sqlalchemy.exc import SQLAlchemyError
 
+from blackcap.blocs.schedule import create_schedule
+from blackcap.routes.schedule import schedule_bp
+from blackcap.schemas.api.schedule.post import ScheduleCreate, SchedulePOSTResponse
 
-@job_bp.post("/")
-def post_job() -> Response:
-    """Post job.
+
+@schedule_bp.post("/")
+def post_schedule() -> Response:
+    """Post schedule.
 
     Returns:
         Response: Flask response
     """
     # Parse json from request
     try:
-        job_create = JobCreate.parse_obj(request.json)
+        schedule_create = ScheduleCreate.parse_obj(request.json)
     except ValidationError as e:
-        response_body = JobPOSTResponse(
-            msg="json validation failed", errors=e.errors()
-        )  # noqa: E501
+        response_body = SchedulePOSTResponse(
+            msg="json validation failed", errors={"main": e.errors()}
+        )
         return make_response(response_body.json(), HTTPStatus.BAD_REQUEST)
     except Exception:
-        response_body = JobPOSTResponse(
-            msg="unknown error", errors=["unknown internal error"]
+        response_body = SchedulePOSTResponse(
+            msg="unknown error", errors={"main": ["unknown internal error"]}
         )
-        return make_response(
-            response_body.json(), HTTPStatus.INTERNAL_SERVER_ERROR
-        )  # noqa: E501
+        return make_response(response_body.json(), HTTPStatus.INTERNAL_SERVER_ERROR)
 
-    # ceate job in DB and publish msg
+    # ceate schedule in DB and publish msg
     try:
-        job = create_schedule(job_create)
+        schedule_list = create_schedule(schedule_create)
     except SQLAlchemyError:
-        response_body = JobPOSTResponse(
-            msg="internal databse error", errors=["internal database error"]
+        response_body = SchedulePOSTResponse(
+            msg="internal databse error", errors={"main": ["unknown internal error"]}
         )
-        return make_response(
-            response_body.json(), HTTPStatus.INTERNAL_SERVER_ERROR
-        )  # noqa: E501
+        return make_response(response_body.json(), HTTPStatus.INTERNAL_SERVER_ERROR)
     except Exception:
-        response_body = JobPOSTResponse(
-            msg="unknown error", errors=["unknown internal error"]
+        response_body = SchedulePOSTResponse(
+            msg="unknown error", errors={"main": ["unknown internal error"]}
         )
-        return make_response(
-            response_body.json(), HTTPStatus.INTERNAL_SERVER_ERROR
-        )  # noqa: E501
+        return make_response(response_body.json(), HTTPStatus.INTERNAL_SERVER_ERROR)
 
-    # return created job in response
-    response_body = JobPOSTResponse(
-        msg="job successfully created", items=[job.dict()]
-    )  # noqa: E501
+    # return created schedule in response
+    response_body = SchedulePOSTResponse(
+        msg="schedule successfully created", items={"schedule_list": schedule_list}
+    )
     return make_response(response_body.json(), HTTPStatus.OK)
