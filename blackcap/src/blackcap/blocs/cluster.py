5685eab302c828efc8830826802c8b2c1a6e573a
Ankur Kumar <ank@leoank.me>
2022-03-11 16:22:20 +0000

    update cluster and schedule blocs
    Signed-off-by: Ankur Kumar <ank@leoank.me>

--- blackcap/src/blackcap/blocs/cluster.py
+++ blackcap/src/blackcap/blocs/cluster.py
@@ -7,21 +7,25 @@ from sqlalchemy import select
 
 from blackcap.db import DBSession
 from blackcap.models.cluster import ClusterDB
+from blackcap.schemas.api.cluster.delete import ClusterDelete
 from blackcap.schemas.api.cluster.get import (
     ClusterGetQueryParams,
     ClusterQueryType,
 )
 from blackcap.schemas.api.cluster.post import ClusterCreate
+from blackcap.schemas.api.cluster.put import ClusterUpdate
 from blackcap.schemas.cluster import Cluster
+from blackcap.schemas.user import User
 
 
 def create_cluster(
-    cluster_create_list: List[ClusterCreate],
-) -> List[Cluster]:  # noqa: E501
+    cluster_create_list: List[ClusterCreate], user_creds: User
+) -> List[Cluster]:
     """Create clusters from create cluster request.
 
     Args:
-        cluster_create_list (List[ClusterCreate]): ClusterCreate request
+        cluster_create_list (List[ClusterCreate]): ClusterCreate request list
+        user_creds (User): User credentials.
 
     Raises:
         Exception: error
@@ -32,9 +36,7 @@ def create_cluster(
     with DBSession() as session:
         try:
             cluster_db_create_list: List[ClusterDB] = [
-                ClusterDB(
-                    **cluster_create.cluster.dict(exclude={"cluster_id"})
-                )  # noqa: E501
+                ClusterDB(**cluster_create.cluster.dict())
                 for cluster_create in cluster_create_list
             ]
             ClusterDB.bulk_create(cluster_db_create_list, session)
@@ -48,11 +50,14 @@ def create_cluster(
             raise e
 
 
-def get_clusters(query_params: ClusterGetQueryParams) -> List[Cluster]:
+def get_clusters(
+    query_params: ClusterGetQueryParams, user_creds: User
+) -> List[Cluster]:
     """Query DB for clusters.
 
     Args:
         query_params (ClusterGetQueryParams): Query params from request
+        user_creds (User): User credentials.
 
     Raises:
         Exception: error
@@ -69,14 +74,98 @@ def get_clusters(query_params: ClusterGetQueryParams) -> List[Cluster]:
 
     with DBSession() as session:
         try:
-            cluster_list: List[ClusterDB] = (
-                session.execute(stmt).scalars().all()
-            )  # noqa: E501
-            return [
-                Cluster(cluster_id=obj.id, **obj.to_dict())
-                for obj in cluster_list  # noqa: E501
-            ]
+            cluster_list: List[ClusterDB] = session.execute(stmt).scalars().all()
+            return [Cluster(cluster_id=obj.id, **obj.to_dict()) for obj in cluster_list]
         except Exception as e:
             session.rollback()
             logger.error(f"Unable to fetch clusters due to {e}")
+            raise e
+
+
+def update_cluster(
+    cluster_update_list: List[ClusterUpdate], user_creds: User
+) -> List[Cluster]:
+    """Update cluster in the DB from ClusterUpdate request.
+
+    Args:
+        cluster_update_list (List[ClusterUpdate]): List of ClusterUpdate request
+        user_creds (User): User credentials.
+
+    Raises:
+        Exception: error
+
+    Returns:
+        List[Cluster]: List of Instance of Updated Cluster
+    """
+    stmt = (
+        select(ClusterDB)
+        .where(ClusterDB.protagonist_id == user_creds.user_id)
+        .where(
+            ClusterDB.id.in_(
+                [cluster_update.template_id for cluster_update in cluster_update_list]
+            )
+        )
+    )
+    with DBSession() as session:
+        try:
+            cluster_db_update_list: List[ClusterDB] = (
+                session.execute(stmt).scalars().all()
+            )
+            updated_cluster_list = []
+            for cluster in cluster_db_update_list:
+                for cluster_update in cluster_update_list:
+                    if cluster_update.cluster_id == cluster.id:
+                        cluster_update_dict = cluster_update.dict(exclude_defaults=True)
+                        cluster_update_dict.pop("cluster_id")
+                        updated_cluster = cluster.update(session, **cluster_update_dict)
+                        updated_cluster_list.append(
+                            Cluster(
+                                cluster_id=updated_cluster.id,
+                                **updated_cluster.to_dict(),
+                            )
+                        )
+            return updated_cluster_list
+        except Exception as e:
+            session.rollback()
+            logger.error(f"Unable to update cluster: {cluster.to_dict()} due to {e}")
+            raise e
+
+
+def delete_cluster(
+    cluster_delete_list: List[ClusterDelete], user_creds: User
+) -> List[Cluster]:
+    """Delete cluster in the DB from ClusterDelete request.
+
+    Args:
+        cluster_delete_list (List[ClusterDelete]): List of ClusterDelete request
+        user_creds (User): User credentials.
+
+    Raises:
+        Exception: error
+
+    Returns:
+        List[Cluster]: List of Instance of Deleted Cluster
+    """
+    stmt = (
+        select(ClusterDB)
+        .where(ClusterDB.protagonist_id == user_creds.user_id)
+        .where(
+            ClusterDB.id.in_([cluster.cluster_id for cluster in cluster_delete_list])
+        )
+    )
+    with DBSession() as session:
+        try:
+            cluster_db_delete_list: List[ClusterDB] = (
+                session.execute(stmt).scalars().all()
+            )
+            deleted_cluster_list = []
+            for cluster in cluster_db_delete_list:
+                cluster.delete(session)
+                deleted_cluster_list.append(
+                    Cluster(cluster_id=cluster.id, **cluster.to_dict())
+                )
+            return deleted_cluster_list
+        except Exception as e:
+            session.rollback()
+            logger.error(f"Unable to delete cluster: {cluster.to_dict()} due to {e}")
             raise e
--- blackcap/src/blackcap/blocs/schedule.py
+++ blackcap/src/blackcap/blocs/schedule.py
@@ -1,10 +1,11 @@
 """Schedule BLoCs."""
 
-from datetime import datetime
 from typing import List
 
 
 from logzero import logger
+from pydantic import ValidationError
+from pydantic.error_wrappers import ErrorWrapper
 from sqlalchemy import select
 
 from blackcap.configs import config_registry
@@ -18,9 +19,8 @@ from blackcap.schemas.api.schedule.get import (
 )
 from blackcap.schemas.api.schedule.post import ScheduleCreate
 from blackcap.schemas.api.schedule.put import ScheduleUpdate
-from blackcap.schemas.message import Message, MessageType
 from blackcap.schemas.schedule import Schedule
-from blackcap.tasks.pub_messenger import publish_messenger
+from blackcap.schemas.user import User
 
 config = config_registry.get_config()
 scheduler = scheduler_registry.get_scheduler(config.SCHEDULER)
@@ -32,39 +32,51 @@ scheduler = scheduler_registry.get_scheduler(config.SCHEDULER)
 
 
 def create_schedule(
-    schedule_create_list: List[ScheduleCreate],
+    schedule_create_list: List[ScheduleCreate], user_creds: User
 ) -> List[Schedule]:
     """Create schedules and save to DB from ScheduleCreate requests.
 
     Args:
         schedule_create_list (List[ScheduleCreate]): List of ScheduleCreate request
+        user_creds (User): User credentials.
+
+    Raises:
+        Exception: database error
 
     Returns:
         List[Schedule]: Instance of Schedule from DB
     """
-    schedule_list: List[Schedule] = [
-        scheduler.schedule(schedule_create) for schedule_create in schedule_create_list
-    ]
+    with DBSession() as session:
+        try:
+            schedule_db_create_list: List[ScheduleDB] = [
+                ScheduleDB(
+                    protagonist_id=user_creds.user_id,
+                    **schedule.dict(),
+                )
+                for schedule in schedule_create_list
+            ]
+            ScheduleDB.bulk_create(schedule_db_create_list, session)
+            return [
+                Schedule(schedule_id=obj.id, **obj.to_dict())
+                for obj in schedule_db_create_list
+            ]
+        except Exception as e:
+            session.rollback()
+            logger.error(f"Unable to create schedules: {e}")
+            raise e
 
-    # publish msg to demon
-    for schedule in schedule_list:
-        message = Message(
-            msg_type=MessageType.TO_DEMON_SCHEDULE_MSG,
-            data=schedule.dict(),
-            timestamp=str(datetime.now()),
-        )
-        publish_messenger.delay(message.dict(), schedule.messenger_queue)
 
-    return schedule_list
-
-
-def get_schedules(query_params: ScheduleGetQueryParams) -> List[ScheduleDB]:
+def get_schedules(
+    query_params: ScheduleGetQueryParams, user_creds: User
+) -> List[Schedule]:
     """Query DB for schedules.
 
     Args:
         query_params (ScheduleGetQueryParams): Query params from request
+        user_creds (User): User credentials.
 
     Raises:
+        e: error
         Exception: error
 
     Returns:
@@ -72,68 +84,124 @@ def get_schedules(query_params: ScheduleGetQueryParams) -> List[ScheduleDB]:
     """
     schedule_list: List[ScheduleDB]
     if query_params.query_type == ScheduleQueryType.GET_ALL_SCHEDULES:
-        stmt = select(ScheduleDB)
+        stmt = select(ScheduleDB).where(ScheduleDB.protagonist_id == user_creds.user_id)
     if query_params.query_type == ScheduleQueryType.GET_SCHEDULE_BY_ID:
+        if query_params.schedule_id is None:
+            e = ValidationError(
+                errors=[
+                    ErrorWrapper(ValueError("field required"), "schedule_id"),
+                ],
+                model=ScheduleGetQueryParams,
+            )
+            raise e
         stmt = select(ScheduleDB).where(ScheduleDB.id == query_params.schedule_id)
     if query_params.query_type == ScheduleQueryType.GET_SCHEDULES_BY_PROTAGONIST_ID:
-        stmt = select(ScheduleDB).where(
-            ScheduleDB.protagonist_id == query_params.protagonist_id
+        if query_params.protagonist_id is None:
+            e = ValidationError(
+                errors=[
+                    ErrorWrapper(ValueError("field required"), "protagonist_id"),
+                ],
+                model=ScheduleGetQueryParams,
+            )
+            raise e
+        stmt = (
+            select(ScheduleDB)
+            .where(ScheduleDB.protagonist_id == user_creds.user_id)
+            .where(ScheduleDB.protagonist_id == query_params.protagonist_id)
         )
     if query_params.query_type == ScheduleQueryType.GET_SCHEDULES_BY_CLUSTER_ID:
-        stmt = select(ScheduleDB).where(
-            ScheduleDB.assigned_cluster_id == query_params.cluster_id
+        if query_params.cluster_id is None:
+            e = ValidationError(
+                errors=[
+                    ErrorWrapper(ValueError("field required"), "protagonist_id"),
+                ],
+                model=ScheduleGetQueryParams,
+            )
+            raise e
+        stmt = (
+            select(ScheduleDB)
+            .where(ScheduleDB.protagonist_id == user_creds.user_id)
+            .where(ScheduleDB.assigned_cluster_id == query_params.cluster_id)
         )
 
     with DBSession() as session:
         try:
-            schedule_list: List[ScheduleDB] = session.execute(stmt).scalars().all()
-            return schedule_list
+            schedule_db_list: List[ScheduleDB] = session.execute(stmt).scalars().all()
+            schedule_list: List[Schedule] = [
+                Schedule(schedule_id=obj.id, **obj.to_dict())
+                for obj in schedule_db_list
+            ]
         except Exception as e:
             session.rollback()
             logger.error(f"Unable to fetch schedules due to {e}")
             raise e
-
     return schedule_list
 
 
-def update_schedule(schedule_update: ScheduleUpdate) -> ScheduleDB:
+def update_schedule(
+    schedule_update_list: List[ScheduleUpdate], user_creds: User
+) -> List[Schedule]:
     """Update schedule in the DB from ScheduleUpdate request.
 
     Args:
-        schedule_update (ScheduleUpdate): ScheduleUpdate request
+        schedule_update_list (List[ScheduleUpdate]): ScheduleUpdate request
+        user_creds (User): User credentials.
 
     Raises:
         Exception: error
 
     Returns:
-        ScheduleDB: Instance of Updated Schedule
+        List[ScheduleDB]: List of Instance of Updated Schedule
     """
-    stmt = select(ScheduleDB).where(ScheduleDB.id == schedule_update.schedule_id)
+    stmt = (
+        select(ScheduleDB)
+        .where(ScheduleDB.protagonist_id == user_creds.user_id)
+        .where(
+            ScheduleDB.id.in_(
+                [
+                    schedule_update.schedule_id
+                    for schedule_update in schedule_update_list
+                ]
+            )
+        )
+    )
     with DBSession() as session:
         try:
-            schedule_list: List[ScheduleDB] = session.execute(stmt).scalars().all()
-            # if schedule_list is None:
-            # raise NotFound exception - 404
-            if len(schedule_list) == 1:
-                schedule_update_dict = schedule_update.dict(exclude_defaults=True)
-                schedule_update_dict.pop("schedule_id")
-                updated_schedule = schedule_list[0].update(
-                    session, **schedule_update_dict
-                )
-                return updated_schedule
+            schedule_db_update_list: List[ScheduleDB] = (
+                session.execute(stmt).scalars().all()
+            )
+            updated_schedule_list = []
+            for schedule in schedule_db_update_list:
+                for schedule_update in schedule_update_list:
+                    if schedule_update.schedule_id == schedule.id:
+                        schedule_update_dict = schedule_update.dict(
+                            exclude_defaults=True
+                        )
+                        schedule_update_dict.pop("schedule_id")
+                        updated_schedule = schedule.update(
+                            session, **schedule_update_dict
+                        )
+                        updated_schedule_list.append(
+                            Schedule(
+                                schedule_id=updated_schedule.id,
+                                **updated_schedule.to_dict(),
+                            )
+                        )
+            return updated_schedule_list
         except Exception as e:
             session.rollback()
-            logger.error(
-                f"Unable to update schedule: {schedule_update.dict()} due to {e}"
-            )
+            logger.error(f"Unable to update schedule: {schedule.to_dict()} due to {e}")
             raise e
 
 
-def delete_schedule(schedule_delete: ScheduleDelete) -> ScheduleDB:
+def delete_schedule(
+    schedule_delete_list: List[ScheduleDelete], user_creds: User
+) -> List[ScheduleDB]:
     """Delete schedule in the DB from ScheduleDelete request.
 
     Args:
-        schedule_delete (ScheduleDelete): ScheduleDelete request
+        schedule_delete_list (List[ScheduleDelete]): List of ScheduleDelete request
+        user_creds (User): User credentials.
 
     Raises:
         Exception: error
@@ -141,21 +209,30 @@ def delete_schedule(schedule_delete: ScheduleDelete) -> ScheduleDB:
     Returns:
         ScheduleDB: Instance of Deleted Schedule
     """
-    stmt = select(ScheduleDB).where(ScheduleDB.id == schedule_delete.schedule_id)
+    stmt = (
+        select(ScheduleDB)
+        .where(ScheduleDB.protagonist_id == user_creds.user_id)
+        .where(
+            ScheduleDB.id.in_(
+                [schedule.schedule_id for schedule in schedule_delete_list]
+            )
+        )
+    )
     with DBSession() as session:
         try:
-            schedule_list: List[ScheduleDB] = session.execute(stmt).scalars().all()
-            if not schedule_list:
-                # TODO: raise not found exception
-                pass
-            if len(schedule_list) == 1:
-                deleted_schedule = schedule_list[0].delete(session)
-                return deleted_schedule
+            schedule_db_delete_list: List[ScheduleDB] = (
+                session.execute(stmt).scalars().all()
+            )
+            deleted_schedule_list = []
+            for schedule in schedule_db_delete_list:
+                schedule.delete(session)
+                deleted_schedule_list.append(
+                    Schedule(schedule_id=schedule.id, **schedule.to_dict())
+                )
+            return deleted_schedule_list
         except Exception as e:
             session.rollback()
-            logger.error(
-                f"Unable to delete schedule: {schedule_delete.dict()} due to {e}"
-            )
+            logger.error(f"Unable to delete schedule: {schedule.to_dict()} due to {e}")
             raise e
 
 
